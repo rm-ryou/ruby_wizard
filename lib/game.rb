5109872f3990665ec72968741ade90419dc0b0ee
Ryo Moriya <ryo.moriya@satori.team>
2023-05-22 14:30:07 +0900

    add game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,33 @@
 class Game
-  def game
-    loop do
-      line = get_input
-      puts line
-    end
+  def initialize(engine)
+    @engine = engine
+  end
+
+  def start
+    line = get_input
+    puts line
+    @engine.exec(line)
+    start
   end
 
   private
 
   def get_input
-    stty_save = `stty -g`.chomp
+    # stty_save = `stty -g`.chomp
     begin
       Readline.readline("> ", true)
     rescue Interrupt
-      system("sttry", stty_save)
+      # system("sttry", stty_save)
       exit
     end
+  end
+
+
+
+
+  def validate_input(line)
+    input_validation = InputValidation.new
+    input_validation.input(line)
+    puts input_validation.message if input_validation.message
   end
 end
